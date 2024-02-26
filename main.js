b25cebd7aa169dcfd5e9c94499c399bf6bc2e420
Vlad Shcherbina <Shherbynavladik998@gmail.com>
2024-02-26 17:34:19 +0200

    calculate

--- main.js
+++ main.js
@@ -1,31 +1,80 @@
-const table = document.querySelector('#myTable')
-let isTrue = true
+let a = ''; //first number
+let b = ''; //second number
+let sign = '';
+let finish = false
 
-for(i = 0; i < 3; i++) {
-    let tr = document.createElement('tr');
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+const action = ['-', '+', 'X', '/']
 
-    for(j = 0; j < 3; j++) {
-        let td = document.createElement('td');
-        td.style.fontSize = '40px'
-        td.style.textAlign = 'center' 
-        td.style.border = '2px solid #000000'
-        td.style.width = '400px'
-        td.style.height = '300px'
-		tr.appendChild(td);
-        td.addEventListener('click', () => {
-            if(isTrue) {
-                td.textContent = 'X'
-                isTrue = false 
-            } else {
-                td.textContent = 'O'
-                isTrue = true
-            }
-            // isTrue = !isTrue
-        })
-        
-    }
-    
+//monitor
 
-    table.appendChild(tr);
+const out = document.querySelector('.calc-screen p')
+function clearAll() {
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
 }
 
+document.querySelector('.ac').onclick = clearAll
+document.querySelector('.buttons').onclick = (event) => {
+   
+    if(!event.target.classList.contains('btn')) return;
+   
+    if(event.target.classList.contains('ac')) return;
+   
+    out.textContent = '';
+    const key = event.target.textContent;
+    if(digit.includes(key)) {
+        if(b === '' && sign === ''){
+        a += key;
+        
+        out.textContent = a
+        } 
+        else if(a!== '' && b!== '' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        } 
+        else{
+            b += key;
+            out.textContent = b;
+        }
+        return;
+    }
+
+    if(action.includes(key)) {
+        sign = key
+        out.textContent = sign
+        return
+    }
+
+    if(key === '=') {
+        if(b === '') b = a;
+        switch(sign) {
+            case "+":
+                a = (+a) + (+b)
+                break;
+            case "-":
+                a = a - b
+                break;  
+            case "X":
+                a = a * b
+                break;   
+            case "/":
+                if(b === '0') {
+                    out.textContent = 'Ошибка';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return
+                }
+                a = a / b
+                break;       
+        }
+        finish = true;
+        out.textContent = a;
+        console.log(a, b, sign)
+    }
+}
